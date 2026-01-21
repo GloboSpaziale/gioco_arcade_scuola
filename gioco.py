4a954eaff5d8c8d1570fb5292fa45e607b943409
GloboSpaziale <gabriele.paggiaro2010@gmail.com>
2026-01-21 11:49:32 +0100

    aggiunto sfondo e assets torri

--- gioco.py
+++ gioco.py
@@ -4,8 +4,17 @@ LARGHEZZA = 350
 ALTEZZA = 600
 class Gioco(arcade.Window):
     def __init__(self):
-        super().__init__(LARGHEZZA, ALTEZZA, "Palline")
+        super().__init__(LARGHEZZA, ALTEZZA, "circa clash")
+        self.altezza = ALTEZZA
+        self.larghezza = LARGHEZZA
         arcade.set_background_color(arcade.color.BLACK)
+        self.background = arcade.load_texture("./assets/sfondo.png")
+    
+    def on_draw(self):
+        arcade.draw_texture_rect(
+            self.background,
+            arcade.LBWH(0,0,self.larghezza,self.altezza)
+        )
 
 def main():
     gioco = Gioco()
