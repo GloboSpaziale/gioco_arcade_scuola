547d77f5e7da1857284168e5aa32239709a30cc0
GloboSpaziale <gabriele.paggiaro2010@gmail.com>
2026-04-07 17:26:41 +0200

    migliorato bot

--- bot.py
+++ bot.py
@@ -12,7 +12,7 @@ def spawna_cattivo(lista, elisir):
 def scelta_cattivo(mazzo):
     valore=8
     lista=[]
-    while valore>=7 or valore<3:
+    while valore>7:
         lista=random.sample(mazzo,random.randint(1,2))
         print(mazzo)
         print(lista)
@@ -27,21 +27,21 @@ def costo(lista):
 
 def mazzo_cattivo ():
     mazzo=[]
-    buono=nemico_globale.Enemy_general(random.randint(0,350),random.randint(325,600),"./assets/cattivo_BN.png",0.75,1,20,1.5,3,3)
+    buono=nemico_globale.Enemy_general(random.randint(0,350),random.randint(325,600),"./assets/cattivo_BN.png",0.75,1,25,1.5,3,3)
     mazzo.append(buono)
-    buono=nemico_globale.Enemy_general(random.randint(0,350),random.randint(325,600), "./assets/combattente_cattivo.png",0.75,0.5,23,1.25,5,3)
+    buono=nemico_globale.Enemy_general(random.randint(0,350),random.randint(325,600), "./assets/combattente_cattivo.png",0.75,0.5,28,1.25,5,3)
     mazzo.append(buono)
-    buono=nemico_globale.Enemy_general(random.randint(0,350),random.randint(325,600), "./assets/danni_cattivo.png",0.65,1,23,1.25,5,4)
+    buono=nemico_globale.Enemy_general(random.randint(0,350),random.randint(325,600), "./assets/danni_cattivo.png",0.65,1,28,1.25,5,4)
     mazzo.append(buono)
-    buono=nemico_globale.Enemy_general(random.randint(0,350),random.randint(325,600), "./assets/goblin_cattivo.png",0.75,0.5,15,1.5,3,2)
+    buono=nemico_globale.Enemy_general(random.randint(0,350),random.randint(325,600), "./assets/goblin_cattivo.png",0.75,0.5,20,1.5,3,2)
     mazzo.append(buono)
-    buono=nemico_globale.Enemy_general(random.randint(0,350),random.randint(325,600), "./assets/scheletro_cattivo.png",0.75,vita=6,danno=2.5)
+    buono=nemico_globale.Enemy_general(random.randint(0,350),random.randint(325,600), "./assets/scheletro_cattivo.png",0.75,vita=11,danno=2.5)
     mazzo.append(buono)
-    buono=nemico_globale.Enemy_general(random.randint(0,350),random.randint(325,600), "./assets/ragno_cattivo.png",0.75,0.6,17,1,3.5,2)
+    buono=nemico_globale.Enemy_general(random.randint(0,350),random.randint(325,600), "./assets/ragno_cattivo.png",0.75,0.6,22,1,3.5,2)
     mazzo.append(buono)
-    buono=nemico_globale.Enemy_general(random.randint(0,350),random.randint(325,600), "./assets/tank_cattivo.png",0.75 ,0.1,60,2,10.5,7)
+    buono=nemico_globale.Enemy_general(random.randint(0,350),random.randint(325,600), "./assets/tank_cattivo.png",0.75 ,0.1,65,2,10.5,6)
     mazzo.append(buono)
-    buono=nemico_globale.Enemy_general(random.randint(0,350),random.randint(325,600), "./assets/veloce_cattivo.png",0.75,2,30,2,5.5,5)
+    buono=nemico_globale.Enemy_general(random.randint(0,350),random.randint(325,600), "./assets/veloce_cattivo.png",0.75,2,35,2,5.5,5)
     mazzo.append(buono)
     print(mazzo)
     return mazzo
